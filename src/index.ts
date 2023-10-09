9271a52f2a63866d5884c2605331a46bd5ad81ff
mangoplane <devmon1@protonmail.com>
2023-10-09 16:47:10 +1100

    Remove redundant import

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import algosdk, { Algodv2, makeApplicationCreateTxn } from "algosdk";
-import { arc69Mimetypes } from "./constants";
-import {
-  AssetInventoryItem,
-  CreateApplicationConfig,
-  FalseyWallet,
-  SignTxnRequest,
-  SignedTxnInGroup,
-  Wallet,
-} from "./util/types";
-
 import AlgorandUtil from "./util";
 import * as Signer from "./signer";
 
